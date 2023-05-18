eb857eb4570d1102e811b6a0a1bd33b19e899c4d
Louis Brochard <louisbrochard@MacBook-Air-de-Louis-4.local>
2023-05-18 08:13:05 +0200

    First commit

---
+++ pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: louisbrochard <louisbrochard@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 08:10:12 by louisbrocha       #+#    #+#             */
+/*   Updated: 2023/05/18 08:11:05 by louisbrocha      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+// main.c
+
+#endif
